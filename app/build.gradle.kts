53537f3b15f0332ff5bd9b6fa9d552048c05f523
Maciej Śledziński <maciejs.sledzinski@cyfroweszkoly.pl>
2024-12-13 13:28:56 +0100

    całe działające zadanie pierwsze

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.zadaniakotlin"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.zadaniakotlin"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app/src/androidTest/java/com/example/zadaniakotlin/coinOrRollSimulator.kt
+++ app/src/androidTest/java/com/example/zadaniakotlin/coinOrRollSimulator.kt
@@ -1,3 +1,27 @@
+
+interface coinOrDiceRoll {
+
+    fun roll(): String
+
+}
+
+class Coin : coinOrDiceRoll {
+    override fun roll() = arrayOf("Orzeł", "Reszka").random()
+}
+
+class Dice : coinOrDiceRoll {
+    override fun roll() = (1..6).random().toString()
+}
+
+
 fun main(){
+
+    println("Wybierz opcję: (1) Rzut monetą (2) Rzut kością")
+    when (readLine()) {
+        "1" -> println("Wynik rzutu monetą: ${Coin().roll()}")
+        "2" -> println("Wynik rzutu kością: ${Dice().roll()}")
+        else -> println("Niepoprawny wybór!")
+    }
+
 
 }
